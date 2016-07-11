31f300055661e8d392c321b95afa148bf8df2b0e
murtazalw <murtaza_lw@yahoo.com>
2016-07-11 11:50:41 +0300

    c1-1

--- Test Git/src/com/example/testgit/GitMain.java
+++ Test Git/src/com/example/testgit/GitMain.java
@@ -4,6 +4,7 @@ public class GitMain {
 
 	public static void main(String[] args) {
 		System.out.println("c1");
+		System.out.println("c1-1");
 		System.out.println("c2-1.1");
 		System.out.print("c2-1.2");
 		System.out.print("c2-1.3");
